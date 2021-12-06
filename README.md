c476aafc082777397d325461dd1ccd9c68e44675
Chris Cao <utsccy@gmail.com>
2021-12-05 22:07:26 -0500

    update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Chris Cao's portfolio website
 
-very simple website developed by only HTML and CSS built in 1 day
+This is a simple website developed by only HTML and CSS in 1 day.
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/home/')
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
